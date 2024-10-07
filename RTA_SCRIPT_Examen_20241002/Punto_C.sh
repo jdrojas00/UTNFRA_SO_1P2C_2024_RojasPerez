6e3f0f21e0fd54127d5734f2b6ee355a438b02cc
jdrojas00 <joserojas061804@gmail.com>
2024-10-07 20:11:47 -0300

    Fix Punto_C.sh

--- RTA_SCRIPT_Examen_20241002/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_C.sh
@@ -6,9 +6,13 @@ sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$(sudo grep $(whoami) /etc
 sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$(sudo grep $(whoami) /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_A2
 sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$(sudo grep $(whoami) /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_A3
 sudo useradd -m -s /bin/bash -G p1c2_2024_gProfesores -p "$(sudo grep $(whoami) /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_P1
+sudo chown p1c2_2024_A1:p1c2_2024_A1 /Examenes-UTN/alumno1/
 sudo chmod 750 /Examenes-UTN/alumno_1/
+sudo chown p1c2_2024_A2:p1c2_2024_A2 /Examenes-UTN/alumno_2/
 sudo chmod 760 /Examenes-UTN/alumno_2/
+sudo chown p1c2_2024_A3:p1c2_2024_A3 /Examenes-UTN/alumno_3/
 sudo chmod 700 /Examenes-UTN/alumno_3/
+sudo chown p1c2_2024_P1:p1c2_2024_P1 /Examenes-UTN/profesores/
 sudo chmod 775 /Examenes-UTN/profesores/
 
 sudo su p1c2_2024_A1 -c "whoami" >> /Examenes-UTN/alumno_1/validar.txt
