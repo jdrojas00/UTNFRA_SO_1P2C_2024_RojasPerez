8a17b997d17bd417ee6cbd688676ddee1bd43b90
jdrojas00 <joserojas061804@gmail.com>
2024-10-07 19:35:34 -0300

    Fix Punto_F.sh

--- RTA_SCRIPT_Examen_20241002/Punto_F.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_F.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Mi IP Publica es: $(curl -s ifconfig.me; echo)" | tee -a /home/osboxes/repogit/UTNFRA_SO_1P2C_2024_RojasPerez/RTA_ARCHIVOS_Examen_20241002/filtro_avanzado.txt
-echo "Mi usuario es: $(whoami)" | tee -a /home/osboxes/repogit/UTNFRA_SO_1P2C_2024_RojasPerez/RTA_ARCHIVOS_Examen_20241002/filtro_avanzado.txt
-echo "El Hash de mi Usuario es: $(sudo grep osboxes /etc/shadow | awk '{print $2}' FS=':')" | tee -a /home/osboxes/repogit/UTNFRA_SO_1P2C_2024_RojasPerez/RTA_ARCHIVOS_Examen_20241002/filtro_avanzado.txt
-echo "La URL de mi repositorio es: $(cd /home/osboxes/repogit/UTNFRA_SO_1P2C_2024_RojasPerez/; git remote get-url origin)" | tee -a /home/osboxes/repogit/UTNFRA_SO_1P2C_2024_RojasPerez/RTA_ARCHIVOS_Examen_20241002/filtro_avanzado.txt
+echo "Mi IP Publica es: $(curl -s ifconfig.me; echo)" | tee -a $HOME/repogit/UTNFRA_SO_1P2C_2024_RojasPerez/RTA_ARCHIVOS_Examen_20241002/filtro_avanzado.txt
+echo "Mi usuario es: $(whoami)" | tee -a $HOME/repogit/UTNFRA_SO_1P2C_2024_RojasPerez/RTA_ARCHIVOS_Examen_20241002/filtro_avanzado.txt
+echo "El Hash de mi Usuario es: $(sudo grep $(whoami) /etc/shadow | awk '{print $2}' FS=':')" | tee -a $HOME/repogit/UTNFRA_SO_1P2C_2024_RojasPerez/RTA_ARCHIVOS_Examen_20241002/filtro_avanzado.txt
+echo "La URL de mi repositorio es: $(cd $HOME/repogit/UTNFRA_SO_1P2C_2024_RojasPerez/; git remote get-url origin)" | tee -a $HOME/repogit/UTNFRA_SO_1P2C_2024_RojasPerez/RTA_ARCHIVOS_Examen_20241002/filtro_avanzado.txt
