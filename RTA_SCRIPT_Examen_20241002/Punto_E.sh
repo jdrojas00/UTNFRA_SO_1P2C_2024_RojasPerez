0a34e9c0aaa3d9faeb4fd8d18f0b932813800026
jdrojas00 <joserojas061804@gmail.com>
2024-10-07 19:49:24 -0300

    Fix Punto_E.sh

--- RTA_SCRIPT_Examen_20241002/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_E.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir $HOME/UTNFRA_SO_1P2C_2024_RojasPerez/RTA_ARCHIVOS_Examen_20241002
-grep -i "memtotal" /proc/meminfo | tee -a $HOME/UTNFRA_SO_1P2C_2024_RojasPerez/RTA_ARCHIVOS_Examen_20241002/Filtro_Basico.txt
-sudo dmidecode -t chassis | tail -n 12 | head -n 2 | tee -a $HOME/UTNFRA_SO_1P2C_2024_RojasPerez/RTA_ARCHIVOS_Examen_20241002/Filtro_Basico.txt
+mkdir $HOME/repogit/UTNFRA_SO_1P2C_2024_RojasPerez/RTA_ARCHIVOS_Examen_20241002
+grep -i "memtotal" /proc/meminfo | tee -a $HOME/repogit/UTNFRA_SO_1P2C_2024_RojasPerez/RTA_ARCHIVOS_Examen_20241002/Filtro_Basico.txt
+sudo dmidecode -t chassis | tail -n 12 | head -n 2 | tee -a $HOME/repogit/UTNFRA_SO_1P2C_2024_RojasPerez/RTA_ARCHIVOS_Examen_20241002/Filtro_Basico.txt
