79f142fc94d7143fdb1da0b5c4efcce5b38e7128
jdrojas00 <joserojas061804@gmail.com>
2024-10-07 19:39:44 -0300

    Fix Punto_C.sh

--- RTA_SCRIPT_Examen_20241002/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_C.sh
@@ -2,10 +2,10 @@
 
 sudo groupadd p1c2_2024_gAlumno
 sudo groupadd p1c2_2024_gProfesores
-sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$(sudo grep osboxes /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_A1
-sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$(sudo grep osboxes /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_A2
-sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$(sudo grep osboxes /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_A3
-sudo useradd -m -s /bin/bash -G p1c2_2024_gProfesores -p "$(sudo grep osboxes /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_P1
+sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$(sudo grep $(whoami) /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_A1
+sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$(sudo grep $(whoami) /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_A2
+sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$(sudo grep $(whoami) /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_A3
+sudo useradd -m -s /bin/bash -G p1c2_2024_gProfesores -p "$(sudo grep $(whoami) /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_P1
 sudo chmod 750 /Examenes-UTN/alumno_1/
 sudo chmod 760 /Examenes-UTN/alumno_2/
 sudo chmod 700 /Examenes-UTN/alumno_3/
