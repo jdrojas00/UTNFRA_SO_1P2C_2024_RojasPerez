bf52d386b3c0150f29e1b964c2a52593c37f2e3f
jdrojas00 <joserojas061804@gmail.com>
2024-10-07 20:29:48 -0300

    Fix Punto_C.sh

--- RTA_SCRIPT_Examen_20241002/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_C.sh
@@ -15,7 +15,7 @@ sudo chmod 700 /Examenes-UTN/alumno_3/
 sudo chown p1c2_2024_P1:p1c2_2024_P1 /Examenes-UTN/profesores/
 sudo chmod 775 /Examenes-UTN/profesores/
 
-sudo su p1c2_2024_A1 -c "whoami" >> /Examenes-UTN/alumno_1/validar.txt
-sudo su p1c2_2024_A2 -c "whoami" >> /Examenes-UTN/alumno_2/validar.txt
-sudo su p1c2_2024_A3 -c "whoami" >> /Examenes-UTN/alumno_3/validar.txt
-sudo su p1c2_2024_P1 -c "whoami" >> /Examenes-UTN/profesores/validar.txt
+sudo su -c "whoami > /Examenes-UTN/alumno_1/validar.txt" p1c2_2024_A1
+sudo su -c "whoami > /Examenes-UTN/alumno_2/validar.txt" p1c2_2024_A2
+sudo su -c "whoami > /Examenes-UTN/alumno_3/validar.txt" p1c2_2024_A3
+sudo su -c "whoami > /Examenes-UTN/profesores/validar.txt" p1c2_2024_P1
